6620633ed841a1249ffb6659a1246d74854fb294
Marcusmalik1 <112938700+Marcusmalik1@users.noreply.github.com>
2023-11-15 16:12:02 +0100

    Update Movie.jsx

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -10,7 +10,7 @@ export default function Movie() {
             <p className="redesign">Innovativ Webapp</p>
           <h2 className="El-header">Movie Magnet</h2>  
           <p>
-            Det nyeste projekt jeg har været inkluderet i er en webapp, hvor vi skulle lave en app til mobiledevice og derefter fetche en api. Her har vi brugt TMDB som vores API, hvor idéen er at brugeren hurtigt og nemt skal finde en film, både med sig selv og et andet individ. Derudover har vi tilpasset webappen så man kan registrere en profil ved hjælp af firebase og firestore, hvor vi kan hente dataer ned fra den enkelte bruger, og blandt andet bruge den til et personligt filmbibliotek.
+            Det nyeste projekt jeg har været inkluderet i er en webapp, hvor vi skulle lave en app til mobiledevice og derefter fetche en api. Her har vi brugt TMDB som vores API, hvor idéen er at brugeren hurtigt og nemt skal finde en film, både med sig selv og et andet individ. Derudover har vi tilpasset webappen så man kan registrere en profil ved hjælp af firebase og firestore, hvor vi kan hente dataer ned fra den enkelte bruger, og blandt andet bruge det til et personligt filmbibliotek.
           </p>
           <div className="website-btn-container">
           <a href="https://moviemagnet-esbenlund.vercel.app/" target="_blank" rel="noopener noreferrer" className="website-btn">Besøg hjemmesiden!</a>
