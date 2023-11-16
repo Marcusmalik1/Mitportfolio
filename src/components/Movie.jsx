b33fa7f59f8e281d7752bc9c0adc95eb71129041
Marcusmalik1 <112938700+Marcusmalik1@users.noreply.github.com>
2023-11-16 13:48:01 +0100

    Update Movie.jsx

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,7 +7,7 @@ export default function Movie() {
     return (
       <section className="elrecycling-container animated">
         <div className="elrecycling-text">
-            <p className="redesign">Innovativ Webapp</p>
+            <p className="redesign">Innovativ Webapp (Programmeret til Mobile Device)</p>
           <h2 className="El-header">Movie Magnet</h2>  
           <p>
             Det nyeste projekt jeg har været inkluderet i er en webapp, hvor vi skulle lave en app til mobiledevice og derefter fetche en api. Her har vi brugt TMDB som vores API, hvor idéen er at brugeren hurtigt og nemt skal finde en film, både med sig selv og et andet individ. Derudover har vi tilpasset webappen så man kan registrere en profil ved hjælp af firebase og firestore, hvor vi kan hente dataer ned fra den enkelte bruger, og blandt andet bruge det til et personligt filmbibliotek.
